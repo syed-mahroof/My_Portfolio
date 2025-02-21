c8364b2a6a91321708e121720a47cca20d889f3f
syed-mahroof <syedmahroof2002@gmail.com>
2025-02-21 16:42:59 +0530

    Final Commit

---
+++ css/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".typing-text", {
+    strings: ["frontend development", "backend development", "web designing", "android development", "web development"],
+    loop: true,
+    typeSpeed: 50,
+    backSpeed: 25,
+    backDelay: 500,
+});
